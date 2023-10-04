a6d5862ffc22d966575174aa00e163e9438e5002
Thalia <97253671+userThaliaoliveira@users.noreply.github.com>
2023-10-04 19:16:36 -0300

    Update api.js

--- receitas/api.js
+++ receitas/api.js
@@ -94,4 +94,9 @@ app.get('/pergunta', (req, res) => {
       res.send("Aqui está uma imagem.");
     }
   });
-  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS' )
+    next();
+});
